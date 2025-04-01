bf8b02107d6cf4c2b7baf5296fe36875854dec4d
Ilya Kislitsyn <i.kislitsyn@g.nsu.ru>
2025-04-02 00:16:23 +0700

    added tests

--- DI-container/src/main/java/ru/nsu/kisadilya/diContainer/example/Gearbox.java
+++ DI-container/src/main/java/ru/nsu/kisadilya/diContainer/example/Gearbox.java
@@ -1,6 +1,14 @@
 package ru.nsu.kisadilya.diContainer.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Gearbox {
+
+    private boolean isManual;
+
     public void shiftUp() {
         System.out.println("Shift up");
     }
--- DI-container/src/test/java/ru/nsu/kisadilya/diContainer/config/MainTest.java
+++ DI-container/src/test/java/ru/nsu/kisadilya/diContainer/config/MainTest.java
@@ -3,13 +3,19 @@ package ru.nsu.kisadilya.diContainer.config;
 
 import org.junit.jupiter.api.Test;
 import ru.nsu.kisadilya.diContainer.Cocina;
-import ru.nsu.kisadilya.diContainer.example.InterfaceExample.Engine1;
+import ru.nsu.kisadilya.diContainer.example.Gearbox;
 import ru.nsu.kisadilya.diContainer.example.InterfaceExample.InterfaceCar;
 import ru.nsu.kisadilya.diContainer.example.InterfaceExample.InterfaceEngine;
 import ru.nsu.kisadilya.diContainer.example.NamedExample.CoolEngine;
-import ru.nsu.kisadilya.diContainer.example.NamedExample.Engine;
 import ru.nsu.kisadilya.diContainer.example.NamedExample.NamedCar;
 import ru.nsu.kisadilya.diContainer.example.SimpleExample.Car;
+import ru.nsu.kisadilya.diContainer.example.ThreadExample;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -50,5 +56,40 @@ public class MainTest {
 //        assertEquals(Engine1.class, interfaceCar.getEngine().getClass());
         assertEquals(InterfaceEngine.class, interfaceCar.getEngine().getClass());
         interfaceCar.run();
+    }
+
+    @Test
+    public void prototypeTest() {
+        Cocina cocina = new Cocina("src/main/java/ru/nsu/kisadilya/diContainer/example/config.json");
+
+        var gearboxProvider = cocina.getIngredienteProvider(Gearbox.class);
+
+        Gearbox gearbox = gearboxProvider.get();
+        Gearbox gearbox2 = gearboxProvider.get();
+
+        assertNotSame(gearbox, gearbox2);
+
+        gearbox2.setManual(false);
+        gearbox.setManual(true);
+
+        assertNotSame(gearbox.isManual(), gearbox2.isManual());
+    }
+
+    @Test
+    public void threadScopeTest() throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        Cocina cocina = new Cocina("src/main/java/ru/nsu/kisadilya/diContainer/example/config.json");
+
+        ThreadExample ex = cocina.getIngrediente("ThreadScopeExample");
+
+        Callable<String> task1 = ex::getValue;
+        Callable<String> task2 = ex::getValue;
+
+        Future<String> result1 = executor.submit(task1);
+        Future<String> result2 = executor.submit(task2);
+        assertNotNull(result1.get(), result2.get());
+
+
     }
 }
